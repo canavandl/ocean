5fcb06474e1c3d21723f2ce3c1ce3af37387198e
Luke Canavan <canavandl@gmail.com>
2014-11-12 12:04:56 -0800

    first wavelength and value api endpoints

--- app/apis.py
+++ app/apis.py
@@ -15,6 +15,7 @@ class Settings(Resource):
     """
     def get(self):
         raise NotImplementedError
+
     def post(self):
         raise NotImplementedError
 
@@ -26,8 +27,19 @@ class AcquireSpectrum(Resource):
                                     DAEMON_CONSTANTS.get(port))
         socketclient.send_message(message)
         values = socketclient.receive_message()
+        socketclient.close_connection()
         return values
 
+class AcquireWavelengths(Resource):
+    def get(self):
+        message = create_sts_command(STS_INTERFACE.get('get_wavelengths'))
+        socketclient = SocketClient(DAEMON_CONSTANTS.get(hostname),
+                                    DAEMON_CONSTANTS.get(port))
+        socketclient.send_message(message)
+        wavelengths = socketclient.receive_message()
+        socketclient.close_connection()
+        return wavelengths
 
-
+api.add_resource(Settings, '/api/settings')
 api.add_resource(AcquireSpectrum, '/api/acquire_spectrum')
+api.add_resource(AcquireWavelengths, '/api/acquire_wavelengths')
