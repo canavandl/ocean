5a434277a8b9051d7197d2985dff2fd06eb8bcff
Luke Canavan <canavandl@gmail.com>
2014-11-14 10:51:29 -0800

    chart update

--- app/views.py
+++ app/views.py
@@ -4,11 +4,4 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    settings = {"max_int": 500,
-                "min_int": 0}
-    return render_template("index.html",
-                           settings=settings,
-                           title="Spectrometer")
-
-#@app.route('/spectra',)
-#def spectra():
+    return render_template("index.html")
